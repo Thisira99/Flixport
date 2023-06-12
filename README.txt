fe86b519d0869b256d14619cf888f194d7d4fbd8
Thisira Thilakshana Jayasekara <90537716+Thisira99@users.noreply.github.com>
2023-06-12 19:43:11 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,17 @@
+1)Navbar Section: This section includes a logo, a menu icon, and a navigation menu with links to different sections of the website. It also contains a dark mode toggle button.
+
+2)Home Section: This section represents the main landing area of the website. It includes a social media links section, an image, a heading, a subheading, a description, and a button to download the CV.
+
+3)About Section: This section provides information about the web developer. It includes an image, a description, a button to toggle achievements, and contact information.
+
+4)Skills Section: This section showcases the web developer's skills. It includes progress bars for HTML, CSS, JavaScript, and Figma.
+
+5)Services Section: This section highlights the services offered by the web developer, such as web development, backend development, UI/UX design, and app development.
+
+6)Contact Section: This section includes a contact form with fields for name, email, and message.
+
+7)Footer Section: This section displays social media icons and a copyright notice.
+
+8)JavaScript: The code includes JavaScript functions for toggling achievements, menu visibility, and dark mode.
+
+9)CSS: The code also includes CSS styles for the different sections and elements of the website.
---
+++ script.js
@@ -0,0 +1,71 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+      var nameInput = document.getElementById('name');
+      var emailInput = document.getElementById('email');
+      var messageInput = document.getElementById('message');
+      
+      // Perform validation checks on the input fields
+      if (nameInput.value.trim() === '') {
+          alert('Please enter your name.');
+          return;
+      }
+      
+      if (emailInput.value.trim() === '') {
+          alert('Please enter your email.');
+          return;
+      }
+      
+      if (messageInput.value.trim() === '') {
+          alert('Please enter a message.');
+          return;
+      }
+      
+      // Submit the form
+      contactForm.submit();
+  });
+});
+// Example - Fade in elements on page load
+const fadeElems = document.querySelectorAll('.fade-in');
+const options = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+const fadeInOnScroll = new IntersectionObserver(function(entries,
+  fadeInOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      fadeInOnScroll.unobserve(entry.target);
+    }
+  });
+}, options);
+fadeElems.forEach(fadeElem => {
+  fadeInOnScroll.observe(fadeElem);
+});
